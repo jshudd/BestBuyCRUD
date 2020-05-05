4e01809e3eceaf60c0c1e9d92c710d71283ff1d2
jshudd <jshudd@gmail.com>
2020-05-05 08:41:43 -0500

    setup ignore

---
+++ BestBuyCRUD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestBuyCRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
